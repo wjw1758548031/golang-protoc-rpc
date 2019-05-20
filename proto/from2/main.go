957e89fbddc041a0a419066f5b9505765ab6635b
wjw1758548031 <46154774+wjw1758548031@users.noreply.github.com>
2019-05-20 15:25:24 +0800

    Update main.go

--- proto/from2/main.go
+++ proto/from2/main.go
@@ -12,7 +12,7 @@ import (
 
 
 func main(){
-	//hello.client这里填写结构的名称 hello为结构 client为方法或者直接写client也行
+	//hello.client 要么是结构名，要么是rpc里通道定义的名称
 	service := micro.NewService(micro.Name("hello.client"))//客户端名称
 	service.Init()
 	// Create new client hello_world为服务名称
