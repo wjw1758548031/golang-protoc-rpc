6d4cd2a8acb0eaa92b52ab29f18454e597f5d92b
wjw1758548031 <46154774+wjw1758548031@users.noreply.github.com>
2019-05-20 14:44:54 +0800

    Update main.go

--- proto/from2/main.go
+++ proto/from2/main.go
@@ -12,9 +12,10 @@ import (
 
 
 func main(){
+	//hello.client这里填写结构的名称 hello为结构 client为方法或者直接写client也行
 	service := micro.NewService(micro.Name("hello.client"))//客户端名称
 	service.Init()
-	// Create new client
+	// Create new client hello_world为服务名称
 	base := pb.NewHelloWorldService("hello_world", service.Client())
 	res, err := base.Hello(context.TODO(), &hello_world.HelloRequest{Name:"wjw"})
 	fmt.Println("res",res)
